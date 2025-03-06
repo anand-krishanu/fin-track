bcab740d8805d331c647e323e54cf499c7495e49
anand-krishanu <anandkrishanu26@gmail.com>
2025-03-06 20:16:19 +0530

    Backend setup

---
+++ fin-track-BACKEND/src/main/java/com/anand/fin_track_BACKEND/FinTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anand.fin_track_BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTrackBackendApplication.class, args);
+	}
+
+}
---
+++ fin-track-BACKEND/src/test/java/com/anand/fin_track_BACKEND/FinTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.fin_track_BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
