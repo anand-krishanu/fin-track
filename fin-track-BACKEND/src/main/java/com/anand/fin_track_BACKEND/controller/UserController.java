c65822f395572ac936499201259022d04534a613
anand-krishanu <anandkrishanu26@gmail.com>
2025-04-02 19:50:30 +0530

    User Repo, service, controler

---
+++ fin-track-BACKEND/src/main/java/com/anand/fin_track_BACKEND/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.anand.fin_track_BACKEND.controller;
+
+import com.anand.fin_track_BACKEND.dto.UserRequest;
+import com.anand.fin_track_BACKEND.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById (@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllUsers () {
+        return ResponseEntity.ok(userService.getAllUser());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUserById (@PathVariable Long id, @RequestBody UserRequest userRequest) {
+        return ResponseEntity.ok(userService.updateUser(id, userRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUserById (@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User Deleted! ");
+    }
+}
---
+++ fin-track-BACKEND/src/main/java/com/anand/fin_track_BACKEND/service/UserService.java
@@ -0,0 +1,69 @@
+package com.anand.fin_track_BACKEND.service;
+
+import com.anand.fin_track_BACKEND.dto.UserRequest;
+import com.anand.fin_track_BACKEND.entity.Family;
+import com.anand.fin_track_BACKEND.entity.User;
+import com.anand.fin_track_BACKEND.repository.FamilyRepository;
+import com.anand.fin_track_BACKEND.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private FamilyRepository familyRepository;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    // Save User with Encoding the password
+    public User saveUser(User user, Long familyId) {
+
+        Family family = familyRepository.findById(familyId)
+                .orElseThrow(() -> new RuntimeException("Family not found"));
+
+        user.setFamily(family);
+        user.setPassword(encoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUser () {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById (Long id) {
+        return Optional.ofNullable(userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User Not Found!")));
+    }
+
+    public User updateUser(Long id, UserRequest userRequest) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setName(userRequest.getName());
+        user.setEmail(userRequest.getEmail());
+        user.setRole(User.Role.valueOf(userRequest.getRole()));
+
+        if (userRequest.getPassword() != null && !userRequest.getPassword().isEmpty()) {
+            user.setPassword(encoder.encode(userRequest.getPassword()));
+        }
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser (Long id) {
+        userRepository.deleteById(id);
+    }
+}
