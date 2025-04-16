1ed508e2c717862585c96ba1930d4dacf401b622
anand-krishanu <anandkrishanu26@gmail.com>
2025-04-16 22:11:23 +0530

    added Validation in DTO

--- fin-track-BACKEND/src/main/java/com/anand/fin_track_BACKEND/dto/UserRequest.java
+++ fin-track-BACKEND/src/main/java/com/anand/fin_track_BACKEND/dto/UserRequest.java
@@ -1,14 +1,32 @@
 package com.anand.fin_track_BACKEND.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class UserRequest {
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
     private String email;
+
+    @NotBlank(message = "Name is mandatory")
     private String name;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotBlank(message = "Role is mandatory")
     private String role;
+
+    @NotNull(message = "Family ID is mandatory")
     private Long familyId;
 }
