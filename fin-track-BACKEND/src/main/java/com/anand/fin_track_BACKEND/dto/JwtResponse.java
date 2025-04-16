370437e6f248d3146a25639513d76cc3d0d9a84f
anand-krishanu <anandkrishanu26@gmail.com>
2025-04-16 22:10:05 +0530

    added DTOs

---
+++ fin-track-BACKEND/src/main/java/com/anand/fin_track_BACKEND/dto/JwtResponse.java
@@ -0,0 +1,10 @@
+package com.anand.fin_track_BACKEND.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+}
